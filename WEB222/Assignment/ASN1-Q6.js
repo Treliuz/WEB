7b905459c0b64c6ab77a432b15942d9519af8187
Treliuz <jmukuna15@gmail.com>
2024-02-12 19:01:38 -0500

    Final final rendition for assignment 1

--- WEB222/Assignment/ASN1-Q6.js
+++ WEB222/Assignment/ASN1-Q6.js
@@ -1,3 +1,12 @@
+/***************************************************************************** 
+* WEB222 – Assignment 1 
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. 
+* No part of this assignment has been copied manually or electronically from any other source 
+* (including web sites) or distributed to other students. 
+* 
+* Name: Joseph Mwamba-Mukuna Student ID: 163997163 Date: 11/02/2024 
+* 
+******************************************************************************/
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
